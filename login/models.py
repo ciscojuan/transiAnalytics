7a462fb4e1419886d953e787dcbcb6a749185722
Camilo Velasquez <camilo.velasquezp@gmail.com>
2018-11-11 00:45:21 -0500

    app login, template, model

--- login/models.py
+++ login/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    user_name = models.CharField(max_length=30, null=True)
+    password = models.CharField(max_length=30)
+    created_at = models.DateField()
